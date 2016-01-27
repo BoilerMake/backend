707e7d81f98dd594005642cc02030cb578016d90
Nicky Semenza <14nicholasse@gmail.com>
2016-01-27 16:58:49 -0500

    schema changes for user / apps

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('last_name');
             $table->string('email')->unique();
             $table->string('password', 60);
+            $table->string('phone')->nullable();
             $table->timestamps();
         });
     }
