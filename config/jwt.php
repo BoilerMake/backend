0c620a4852cdf6f9caaf1da4ae2d77795aab9f3a
Kedar <kedar54@gmail.com>
2016-06-04 10:06:54 -0400

    add PodPod key command, config

--- config/jwt.php
+++ config/jwt.php
@@ -12,7 +12,7 @@ return [
     |
     */
 
-    'secret' => env('JWT_SECRET', 'A11JuuzeEjacvRvYy2kXlJfBnQAi1VtL'),
+    'secret' => env('JWT_SECRET', 'W0QHrnC1seZL7RDnqzot4LscQSRdZq4u'),
 
     /*
     |--------------------------------------------------------------------------
