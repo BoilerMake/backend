60fe84282dbb0056592a1eb778bc67aa6a7cfaca
Kedar Vaidya <kedar@Kedars-MacBook-Pro.local>
2016-11-21 17:10:40 -0600

    add curl text to email

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,5 +1,5 @@
 Thanks for making an account. You can <a href="{{$link}}">verify your account and finish your application here.</a>
-
+(or if you want, curl api.boilermake.org/v1/users/verify/{{$user->confirmation_code}})
 <br><br>
 
 BoilerMake is a hackathon at Purdue University from January 20-22 that will be out of this world. With anything you could possibly need in your own personal space kit, explorers will need to bring nothing but their laptops, creative minds, and love for learning with them for an intergalactic experience.
