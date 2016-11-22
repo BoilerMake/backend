e23de9f9d9c9b6d61b722faa90dfda8f1be53b51
Nicky Semenza <nicky@nickysemenza.com>
2016-11-22 00:33:50 -0800

    get rid of uncessary middleware codez

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,21 +18,6 @@ Route::get('/fe', function () {
     return env('FRONTEND_ADDRESS');
 });
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| This route group applies the "web" middleware group to every route
-| it contains. The "web" middleware group is defined in your HTTP
-| kernel and includes session state, CSRF protection, and more.
-|
-*/
-
-Route::group(['middleware' => ['web']], function () {
-    //
-});
-
 /**
  * API ROUTES
  */
