8eacb3d8ce4a88e8e708e245c18f4adb6c6b4853
Nicky Semenza <nicky@nickysemenza.com>
2016-11-21 20:50:38 -0800

    test code for print out FE addr (dev vs prod)

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,6 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/fe', function () {
+    return env('FRONTEND_ADDRESS');
+});
 
 /*
 |--------------------------------------------------------------------------
