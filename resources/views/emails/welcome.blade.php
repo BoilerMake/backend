941380f2627ea20654c4249695f94661f4c7f3e6
Nicky Semenza <nicky@nickysemenza.com>
2016-11-21 16:36:37 -0800

    updated welcome emailz

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -3,3 +3,8 @@ Thanks for making an account. You can <a href="{{$link}}">verify your account an
 <br><br>
 
 BoilerMake is a hackathon at Purdue University from January 20-22 that will be out of this world. With anything you could possibly need in your own personal space kit, explorers will need to bring nothing but their laptops, creative minds, and love for learning with them for an intergalactic experience.
+
+<br><br>
+Explore On
+<br>
+-BoilerMake team
