19207c8971df8c32bf34042c00c1c426207fb913
Nicky Semenza <nicky@nickysemenza.com>
2016-12-08 22:45:52 -0500

    Add Facebook event to schools table

---
+++ database/migrations/2016_12_08_222655_add_facebook_event_id_to_schools_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFacebookEventIdToSchoolsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('schools', function (Blueprint $table) {
+            $table->string('facebook_event_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('schools', function (Blueprint $table) {
+            $table->dropColumn('facebook_event_id');
+        });
+    }
+}
