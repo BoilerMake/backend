aabc57f3defb976f8aef81f4f98ddacc858a8b48
Kedar <kedar54@gmail.com>
2017-01-02 14:01:11 -0600

    begin event cleanup

---
+++ database/migrations/2017_01_02_145820_remove_event_name_active_column.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RemoveEventNameActiveColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('events', function (Blueprint $table) {
+            $table->dropColumn('name');
+            $table->dropColumn('active');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
