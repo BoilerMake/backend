c9da24bfa3e9bf65adb34f33eec1294fe52b65be
Nicky Semenza <nicky@nickysemenza.com>
2017-01-05 01:32:13 -0800

    Add transit method column to schools table, defaulting to car 🚗

---
+++ database/migrations/2017_01_05_032949_add_transit_method_to_schools_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTransitMethodToSchoolsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('schools', function (Blueprint $table) {
+            $table->string('transit_method')->default('car');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('schools', function (Blueprint $table) {
+            $table->dropColumn('transit_method');
+        });
+    }
+}
