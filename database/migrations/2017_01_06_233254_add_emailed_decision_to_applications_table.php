395211bf14da46fff7b64d703eeea21b57cfc697
Nicky Semenza <nicky@nickysemenza.com>
2017-01-06 23:35:44 -0800

    add emailed_decision column to Applications table

---
+++ database/migrations/2017_01_06_233254_add_emailed_decision_to_applications_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddEmailedDecisionToApplicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('applications', function (Blueprint $table) {
+            $table->boolean('emailed_decision')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('applications', function (Blueprint $table) {
+            $table->dropColumn('emailed_decision');
+        });
+    }
+}
