44d42e0ee7594cb42783f0c93a93bbdb04413829
Nicky Semenza <nicky@nickysemenza.com>
2017-01-15 01:41:19 -0500

    new output folder

--- app/Http/Controllers/API/UsersController.php
+++ app/Http/Controllers/API/UsersController.php
@@ -283,7 +283,7 @@ class UsersController extends Controller
 
 //            $combined->addImage( $image );
 //            $combined->setImageFormat("jpg");
-            $fileName = 'cards/layout-'.$x.'.pdf';
+            $fileName = 'cards-output/layout-'.$x.'.pdf';
             $path = public_path().'/'.$fileName;
             $image->writeImage($path);
             $x++;
