0eda61ef52266a7b21eef6b7054bddd7ca289990
Nicky Semenza <nicky@nickysemenza.com>
2017-09-25 16:24:58 -0400

    card code cleaning

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -16,13 +16,18 @@ class CardController extends Controller
 {
     const CARD_TYPE_HACKER = 1;
     const CARD_TYPE_EXEC = 2;
-    const CARD_ROLE_STRIPE_EXEC = '#1A4A98';
+
+    const SHEET_WIDTH_PX = 3300;
+    const SHEET_HEIGHT_PX = 2550;
+
+    const CARD_WIDTH_PX = 900;
+    const CARD_HEIGHT_PX = 1200;
 
     /**
      * Stitches user access cards together into a PDF.
      * @param array|null $user_ids
      */
-    public static function stitchAccessCards($user_ids = null, $stripeColor = self::CARD_ROLE_STRIPE_EXEC)
+    public static function stitchAccessCards($user_ids = null)
     {
         if ($user_ids) {
             $paths = User::whereIn('id', $user_ids)->get()->pluck('card_image')->toArray();
@@ -31,88 +36,53 @@ class CardController extends Controller
         }
         $pages = array_chunk($paths, 6);
         $whitePixel = new ImagickPixel('#FFFFFF');
-        $color1 = new ImagickPixel('#24133A');
+        $color1 = new ImagickPixel('#24133A');//todo: based on role
 
-        $x = 0;
+        $pageNum = 0;
         foreach ($pages as $page) {
             $image = new Imagick();
-            $image->newImage(3300, 2550, $color1);
+            $image->newImage(self::SHEET_WIDTH_PX, self::SHEET_HEIGHT_PX, $color1);
             $image->setImageUnits(Imagick::RESOLUTION_PIXELSPERINCH);
             $image->setImageResolution(300, 300);
             $image->setImageFormat('jpg');
 
-            //hammers BG
+            //6xhammers BG
             $item1raw = new Imagick();
             $item1raw->readImageFile(fopen(resource_path('assets/hammers6.png'), 'rb'));
-            $item1raw->cropThumbnailImage(3300, 2550);
+            $item1raw->cropThumbnailImage(self::SHEET_WIDTH_PX, self::SHEET_HEIGHT_PX);
             $image->compositeImage($item1raw, IMAGICK::COMPOSITE_DEFAULT, 0, 0);
 
-            //add role stripe w/ bleed
+            //add some bleed to the role stripe, underneath where 6up cards are placed
             $roleStripe = new ImagickDraw();
             $roleStripe->setFillColor($whitePixel);
-            $roleStripe->rectangle(0, 1085, 3300, 1475);
+            $roleStripe->rectangle(0, 1085, self::SHEET_WIDTH_PX, 1475);
             $image->drawImage($roleStripe);
 
-            if (isset($page[0])) {
-                $card = new Imagick();
-                $card->readImageFile(fopen(public_path().'/'.$page[0], 'rb'));
-                $image->compositeImage($card, IMAGICK::COMPOSITE_DEFAULT, 150, 80);
-                $card->clear();
-                $card->destroy();
+            $cardPositions = [
+                0=>["x"=>150,   "y"=>80],
+                1=>["x"=>1200,  "y"=>80],
+                2=>["x"=>2250,  "y"=>80],
+                3=>["x"=>150,   "y"=>1279],
+                4=>["x"=>1200,  "y"=>1279],
+                5=>["x"=>2250,  "y"=>1279]
+            ];
+            for($cardLayoutPosNum = 0; $cardLayoutPosNum<6; $cardLayoutPosNum++) {
+                if (isset($page[$cardLayoutPosNum])) {
+                    $card = new Imagick();
+                    $card->readImageFile(fopen(public_path().'/'.$page[$cardLayoutPosNum], 'rb'));
+                    $card->rotateimage($whitePixel, $cardLayoutPosNum >= 3 ? 180 : 0); //rotate bottom 3 by 180
+                    $image->compositeImage($card, IMAGICK::COMPOSITE_DEFAULT, $cardPositions[$cardLayoutPosNum]["x"], $cardPositions[$cardLayoutPosNum]["y"]);
+                    $card->clear();
+                    $card->destroy();
+                }
             }
 
-            if (isset($page[1])) {
-                $card = new Imagick();
-                $card->readImageFile(fopen(public_path().'/'.$page[1], 'rb'));
-                $image->compositeImage($card, IMAGICK::COMPOSITE_DEFAULT, 1200, 80);
-                $card->clear();
-                $card->destroy();
-            }
-
-            if (isset($page[2])) {
-                $card = new Imagick();
-                $card->readImageFile(fopen(public_path().'/'.$page[2], 'rb'));
-                $image->compositeImage($card, IMAGICK::COMPOSITE_DEFAULT, 2250, 80);
-                $card->clear();
-                $card->destroy();
-            }
-
-            if (isset($page[3])) {
-                $card = new Imagick();
-                $card->readImageFile(fopen(public_path().'/'.$page[3], 'rb'));
-                $card->rotateimage($whitePixel, 180);
-                $image->compositeImage($card, IMAGICK::COMPOSITE_DEFAULT, 150, 1279);
-                $card->clear();
-                $card->destroy();
-            }
-
-            if (isset($page[4])) {
-                $card = new Imagick();
-                $card->readImageFile(fopen(public_path().'/'.$page[4], 'rb'));
-                $card->rotateimage($whitePixel, 180);
-                $image->compositeImage($card, IMAGICK::COMPOSITE_DEFAULT, 1200, 1279);
-                $card->clear();
-                $card->destroy();
-            }
-
-            if (isset($page[5])) {
-                $card = new Imagick();
-                $card->readImageFile(fopen(public_path().'/'.$page[5], 'rb'));
-                $card->rotateimage($whitePixel, 180);
-                $image->compositeImage($card, IMAGICK::COMPOSITE_DEFAULT, 2250, 1279);
-                $card->clear();
-                $card->destroy();
-            }
-
-
-            //            $combined->addImage( $image );
-            //            $combined->setImageFormat("jpg");
-            $fileName = 'cards-output/layout-'.$x.'.pdf';
+            $fileName = 'cards-output/layout-'.$pageNum.'.pdf';
             $path = public_path().'/'.$fileName;
             $image->writeImage($path);
-            $x++;
             $image->clear();
             $image->destroy();
+            $pageNum++;
         }
     }
 
@@ -122,7 +92,7 @@ class CardController extends Controller
         $bluePixel = new ImagickPixel('#1A4A98');
 
         $image = new Imagick();
-        $image->newImage(3300, 2550, $whitePixel);
+        $image->newImage(self::SHEET_WIDTH_PX, self::SHEET_HEIGHT_PX, $whitePixel);
         $image->setImageUnits(Imagick::RESOLUTION_PIXELSPERINCH);
         $image->setImageResolution(300, 300);
         $image->setImageFormat('jpg');
@@ -130,7 +100,7 @@ class CardController extends Controller
 
         $roleStripe = new ImagickDraw();
         $roleStripe->setFillColor($bluePixel);
-        $roleStripe->rectangle(0, 1275, 3300, 1276);
+        $roleStripe->rectangle(0, 1275, self::SHEET_WIDTH_PX, 1276);
         $image->drawImage($roleStripe);
 
 
@@ -150,6 +120,16 @@ class CardController extends Controller
         $image->writeImage($path);
     }
 
+    /**
+     * @param $skill
+     * @return Imagick the icon
+     */
+    public static function getSizedSkillIcon($skill) {
+        $icon = new Imagick();
+        $icon->readImageFile(fopen(resource_path("assets/language_icons/${skill}.png"), 'rb'));
+        $icon->cropThumbnailImage(130, 130);
+        return $icon;
+    }
     /**
      * Generates an access card image for a given user.
      * @param user $user_id the user's id
@@ -180,87 +160,60 @@ class CardController extends Controller
 
         $moonBold = resource_path('assets/fonts/MoonBold.ttf');
         $moonLight = resource_path('assets/fonts/MoonLight.ttf');
-        $transparentPixel = new ImagickPixel('transparent');
-        /* New image */
-        $fullWidth = 900; //3in @ 300ppi
         $image = new Imagick();
-        $image->newImage($fullWidth, 1200, $transparentPixel);
-//        $image->setImageAlpha(0);
+        $image->newImage(self::CARD_WIDTH_PX, self::CARD_HEIGHT_PX, new ImagickPixel('transparent'));
         $image->setImageUnits(Imagick::RESOLUTION_PIXELSPERINCH);
         $image->setImageResolution(300, 300);
 
-        /* GENERATE SKILLS ICONS */
+        /* SKILLS ICONS */
         $skillRow = $user->application->skills;
         $skills = $skillRow && $skillRow != "null" ? explode(",",substr($skillRow,1,strlen($skillRow)-2)) : [];
-//            explode(",",json_decode($user->application->skills, true));
 
         $skillsYPos = 830;
 
         if (count($skills) == 3) {
-            $item1raw = new Imagick();
-            $item1raw->readImageFile(fopen(resource_path('assets/language_icons/'.$skills[0].'.png'), 'rb'));
-            $item1raw->cropThumbnailImage(130, 130);
-            $image->compositeImage($item1raw, IMAGICK::COMPOSITE_DEFAULT, 250, $skillsYPos);
-
-            $item1raw = new Imagick();
-            $item1raw->readImageFile(fopen(resource_path('assets/language_icons/'.$skills[1].'.png'), 'rb'));
-            $item1raw->cropThumbnailImage(130, 130);
-            $image->compositeImage($item1raw, IMAGICK::COMPOSITE_DEFAULT, 400, $skillsYPos);
-
-            $item1raw = new Imagick();
-            $item1raw->readImageFile(fopen(resource_path('assets/language_icons/'.$skills[2].'.png'), 'rb'));
-            $item1raw->cropThumbnailImage(130, 130);
-            $image->compositeImage($item1raw, IMAGICK::COMPOSITE_DEFAULT, 550, $skillsYPos);
-        }
-        if (count($skills) == 2) {
-            $item1raw = new Imagick();
-            $item1raw->readImageFile(fopen(resource_path('assets/language_icons/'.$skills[0].'.png'), 'rb'));
-            $item1raw->cropThumbnailImage(130, 130);
-            $image->compositeImage($item1raw, IMAGICK::COMPOSITE_DEFAULT, 320, $skillsYPos);
-
-            $item1raw = new Imagick();
-            $item1raw->readImageFile(fopen(resource_path('assets/language_icons/'.$skills[1].'.png'), 'rb'));
-            $item1raw->cropThumbnailImage(130, 130);
-            $image->compositeImage($item1raw, IMAGICK::COMPOSITE_DEFAULT, 475, $skillsYPos);
-        }
-        if (count($skills) == 1) {
-            $item1raw = new Imagick();
-            $item1raw->readImageFile(fopen(resource_path('assets/language_icons/'.$skills[0].'.png'), 'rb'));
-            $item1raw->cropThumbnailImage(130, 130);
-            $image->compositeImage($item1raw, IMAGICK::COMPOSITE_DEFAULT, 400, $skillsYPos);
+            $image->compositeImage(self::getSizedSkillIcon($skills[0]), IMAGICK::COMPOSITE_DEFAULT, 250, $skillsYPos);
+            $image->compositeImage(self::getSizedSkillIcon($skills[1]), IMAGICK::COMPOSITE_DEFAULT, 400, $skillsYPos);
+            $image->compositeImage(self::getSizedSkillIcon($skills[2]), IMAGICK::COMPOSITE_DEFAULT, 550, $skillsYPos);
+        } else if (count($skills) == 2) {
+            $image->compositeImage(self::getSizedSkillIcon($skills[0]), IMAGICK::COMPOSITE_DEFAULT, 320, $skillsYPos);
+            $image->compositeImage(self::getSizedSkillIcon($skills[1]), IMAGICK::COMPOSITE_DEFAULT, 475, $skillsYPos);
+        } else if (count($skills) == 1) {
+            $image->compositeImage(self::getSizedSkillIcon($skills[0]), IMAGICK::COMPOSITE_DEFAULT, 400, $skillsYPos);
         }
 
+        /* Add BOILERMAKE text */
         $item1raw = new Imagick();
         $item1raw->readImageFile(fopen(resource_path('assets/boilermaketxt.png'), 'rb'));
         $item1raw->cropThumbnailImage(519, 64);
         $image->compositeImage($item1raw, IMAGICK::COMPOSITE_DEFAULT, 187, 84);
 
+        /* Add Name */
         $namePosition = 420;
-
         $nameTextLine = new ImagickDraw();
         $nameTextLine->setFont($moonBold);
         $nameTextLine->setTextAlignment(\Imagick::ALIGN_CENTER);
         $nameTextLine->setTextKerning(2);
         $nameTextLine->setFontSize(80);
         $nameTextLine->setFillColor($whitePixel);
+        $image->annotateImage($nameTextLine, self::CARD_WIDTH_PX / 2, $namePosition, 0, $user->first_name.' '.$user->last_name);
 
-        $image->annotateImage($nameTextLine, $fullWidth / 2, $namePosition, 0, $user->first_name.' '.$user->last_name);
-
+        /* Add School */
         $schoolTextLine = new ImagickDraw();
         $schoolTextLine->setFont($moonLight);
         $schoolTextLine->setTextAlignment(\Imagick::ALIGN_CENTER);
         $schoolTextLine->setTextKerning(2);
         $schoolTextLine->setFontSize(45);
         $schoolTextLine->setFillColor($whitePixel);
+        $image->annotateImage($schoolTextLine, self::CARD_WIDTH_PX / 2, $namePosition + 60, 0, $schoolName);
 
-        $image->annotateImage($schoolTextLine, $fullWidth / 2, $namePosition + 60, 0, $schoolName);
-
-        //add the stripe
+        /* Add role */
+        //white stripe
         $roleStripe = new ImagickDraw();
         $roleStripe->setFillColor($whitePixel);
-        $roleStripe->rectangle(0, 1007, $fullWidth, 1200);
+        $roleStripe->rectangle(0, 1007, self::CARD_WIDTH_PX, self::CARD_HEIGHT_PX);
         $image->drawImage($roleStripe);
-
+        //add role text
         $roleTextLine = new ImagickDraw();
         $roleTextLine->setFont($moonBold);
         $roleTextLine->setTextAlignment(\Imagick::ALIGN_CENTER);
@@ -268,8 +221,10 @@ class CardController extends Controller
         $roleTextLine->setFontSize(70);
         $roleTextLine->setFillColor($blackPixel);
         $roleText = 'HACKER';
-        $image->annotateImage($roleTextLine, $fullWidth / 2, 1120, 0, $roleText);
+        $image->annotateImage($roleTextLine, self::CARD_WIDTH_PX / 2, 1120, 0, $roleText);
 
+
+        /* SAVE! */
         $fileName = 'cards/card_'.$cardType.'_'.$user_id.'.png';
         $path = public_path().'/'.$fileName;
         $user->card_image = $fileName;
