a65e417ef12aab0c8471f4445a4d095ad9a23847
MaxLin <maxylin@gmail.com>
2018-09-26 16:23:49 -0400

    StyleCI fix

--- database/migrations/2018_09_26_133325_add_team_to_users.php
+++ database/migrations/2018_09_26_133325_add_team_to_users.php
@@ -13,7 +13,7 @@ class AddTeamToUsers extends Migration
      */
     public function up()
     {
-        Schema::table('users', function(Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->text('team_names');
         });
     }
@@ -25,7 +25,7 @@ class AddTeamToUsers extends Migration
      */
     public function down()
     {
-        Schema::table('users', function(Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('team_names');
         });
     }
--- database/migrations/2018_09_26_133355_add_project_name_to_users.php
+++ database/migrations/2018_09_26_133355_add_project_name_to_users.php
@@ -13,7 +13,7 @@ class AddProjectNameToUsers extends Migration
      */
     public function up()
     {
-        Schema::table('users', function(Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->text('project_idea');
         });
     }
@@ -26,7 +26,7 @@ class AddProjectNameToUsers extends Migration
     public function down()
     {
         //
-        Schema::table('users', function(Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('project_idea');
         });
     }
